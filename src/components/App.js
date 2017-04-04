0409d898ff36826faef640c35a66c0f1b413a8a3
Kristy Yeaton <kristyjy@gmail.com>
2017-04-03 20:25:01 -0400

    WIP Add react slingshot instead of react redux starter kit

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React, { PropTypes } from 'react';
+import Header from './Header';
+
+// This is a class-based component because the current
+// version of hot reloading won't hot reload a stateless
+// component at the top-level.
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  children: PropTypes.element
+};
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { IndexLink } from 'react-router';
+import { Navbar } from 'reactstrap';
+
+export default class Header extends React.Component {
+
+  render() {
+    return (
+      <Navbar color="faded" light toggleable>
+        <IndexLink to='/' className='navbar-brand'>
+          Battle Plan
+        </IndexLink>
+      </Navbar>
+    );
+  }
+}
